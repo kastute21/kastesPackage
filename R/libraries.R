db9c715eae23891075b38b4583c4beda26d3a616
kastute21 <kaste.ivanauskaite@gmail.com>
2025-01-24 20:10:02 +0100

    Update libraries.R

--- R/libraries.R
+++ R/libraries.R
@@ -1,19 +1,13 @@
 libraries <- function(x = all) {
   main <- c("ggplot2", "dplyr", "tidyr","MASS")
-  fun <- c("memer", "cowsay", "wesanderson")
   plot <- c("plotly", "ggplot2", "gganimate", "ggstatsplot")
-  all <- c(main, fun, plot)
+  all <- c(main, plot)
   to_install <- all[!all %in% rownames(installed.packages())]
   if (length(to_install) > 0) {
     install.packages(to_install)
   }
   if ("main" %in% x) {
     for (pkg in main) {
-      library(pkg, character.only = T)
-    }
-  }
-  if ("fun" %in% x) {
-    for (pkg in fun) {
       library(pkg, character.only = T)
     }
   }
