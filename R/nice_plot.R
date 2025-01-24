4b2f5f0e09d804a3da385f82ed464a43bb4b2280
kastute21 <kaste.ivanauskaite@gmail.com>
2025-01-24 20:29:56 +0100

    Update nice_plot.R

--- R/nice_plot.R
+++ R/nice_plot.R
@@ -7,8 +7,7 @@ nice_plot <- function(x, y, plot_type = "scatter", title = "Title", xlab = "X Ax
   } else if (plot_type == "line") {
     plot(x, y, type = "l", main = title, xlab = xlab, ylab = ylab, col = wes_palette(name=sample(palettes,1),1), lwd = 2)
   } else if (plot_type == "hist") {
-    breaks <- max(10, floor(sqrt(length(x))))
-    hist(x, main = title, xlab = xlab, col = wes_palette(name=sample(palettes,1)), breaks = breaks)
+    hist(x, main = title, xlab = xlab, col = wes_palette(name=sample(palettes,1)))
   } else {
     stop("Error: invalid plot type")
   }
